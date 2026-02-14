2d059e45fd485bfa777f5830888762f456446607
Zarar Ahmed <Ahmedzarar74@gmail.com>
2026-02-14 11:30:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+document.querySelector("#btn-1").addEventListener("click" , function () {
+  document.body.style.backgroundColor = "red"
+});
+document.querySelector("#btn-2").addEventListener("click" , function () {
+  document.body.style.backgroundColor = "Blue"
+});
+document.querySelector("#btn-3").addEventListener("click", function() {
+  document.body.style.backgroundColor = "Green"
+});
+document.querySelector("#btn-4").addEventListener("click", function() {
+  document.body.style.backgroundColor = "Orange"
+});
+document.querySelector("#btn-5").addEventListener("click", function() {
+  document.body.style.backgroundColor = "White"
+});
